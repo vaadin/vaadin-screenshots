c2119753a471595d787b9cdb87976260e5674f09
Leif Åstrand <leif@vaadin.com>
2013-03-13 10:06:27 +0200

    Script for removing screenshots for untested browsers

---
+++ pruneOldBrowsers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# When copying screenshots from the 6.8 repository, this script can be used to easily remove screenshots for browsers that we no longer test for.
+
+rm -v reference/*_Windows_Chrome_13_*
+rm -v reference/*_Windows_Firefox_3_*
+rm -v reference/*_Windows_InternetExplorer_6_*
+rm -v reference/*_Windows_InternetExplorer_7_*
+rm -v reference/*_Windows_Opera_10_*
+rm -v reference/*_Windows_Safari_4_*
+
+
