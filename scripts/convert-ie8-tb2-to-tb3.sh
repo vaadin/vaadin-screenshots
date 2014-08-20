c81c0646e8385bf822bb713a8db3211f0d1e517f
Artur Signell <artur@vaadin.com>
2014-08-20 14:36:48 +0300

    Fix missing directory
    Change-Id: Id46464bbb8185b82104a14b7a7d8f481b6d2a424

--- scripts/convert-ie8-tb2-to-tb3.sh
+++ scripts/convert-ie8-tb2-to-tb3.sh
@@ -20,6 +20,6 @@ for a in $*
 do
 	convert $a -crop 1496x846+0+0 png:$script/cropped.png
 	convert $script/ie8-topborder-narrow.png -append $script/cropped.png -append $script/ie8-bottomborder-narrow.png -append png:$script/withtopbottom.png
-	convert $script/ie8-leftborder.png +append $script/withtopbottom.png +append ie8-rightborder.png +append png:$a
+	convert $script/ie8-leftborder.png +append $script/withtopbottom.png +append $script/ie8-rightborder.png +append png:$a
 	
 done
