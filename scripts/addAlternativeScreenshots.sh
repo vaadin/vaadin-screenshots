5b72952f6136ed6119bab160d470d1afa98ec925
Artur Signell <artur@vaadin.com>
2015-07-12 20:55:44 +0300

    Script for copying given files as new references, leaving any old version in place
    Change-Id: Idfb225159d88f6b85e68c6507ca5d39ede914121

---
+++ scripts/addAlternativeScreenshots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" = "0" ]
+then
+        echo "Usage: $0 <screenshots to add>"
+        exit 1
+fi
+
+
+refdir=`dirname $0`/../reference
+
+for filepath in $*
+do
+        file=`basename $filepath`
+        target="$refdir/$file"
+        
+        alt=0
+        
+        while [ -e $target ]
+        do
+                alt=$(($alt+1))
+                target="$refdir/"`echo $file|sed "s/.png/_$alt.png/g"`
+        done
+        
+        cp "$file" "$target"
+done
